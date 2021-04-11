4040fae69a944f4d01d162d4af893d38ab47bd40
ADMIN <naqeeb249@gmail.com>
2021-04-11 14:30:39 +0530

    DOM

---
+++ DOM/main.js
@@ -0,0 +1,51 @@
+
+const btn = document.querySelector('.btn');
+
+/*btn.addEventListener('click',(e) => {
+    (e).preventDefault();
+    document.querySelector('#my-form').style.background = '#ccc';
+
+    document.querySelector('body').classList.add('bg-dark');
+});*/
+
+btn.addEventListener('mouseover', (e) => {
+    (e).preventDefault();
+    btn.style.background = 'red';
+})
+
+btn.addEventListener('mouseout',(e) => {
+    (e).preventDefault();
+    btn.style.background = 'black';
+})
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+    
+    if(nameInput.value === '' || emailInput.value === ''){
+        msg.classList.add('error');
+        msg.innerHTML = 'Please fill all fields';
+
+        setTimeout(()  => msg.remove(), 3000)
+    }
+    else{
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        userList.appendChild(li);
+
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
+
+
+
+
+
