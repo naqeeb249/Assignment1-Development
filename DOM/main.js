2ed0d21389e23b176bc696d0d77cbe7d20ffec53
ADMIN <naqeeb249@gmail.com>
2021-04-28 20:27:26 +0530

    Deleting The User Detail Of Saved User

--- DOM/main.js
+++ DOM/main.js
@@ -1,12 +1,12 @@
 
 const btn = document.querySelector('.btn');
-
-/*btn.addEventListener('click',(e) => {
+/* 
+btn.addEventListener('click',(e) => {
     (e).preventDefault();
     document.querySelector('#my-form').style.background = '#ccc';
 
     document.querySelector('body').classList.add('bg-dark');
-});*/
+}); */
 
 btn.addEventListener('mouseover', (e) => {
     (e).preventDefault();
@@ -19,14 +19,13 @@ btn.addEventListener('mouseout',(e) => {
 })
 
 const myForm = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
+
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
 
-myForm.addEventListener('submit', onSubmit);
 
-myForm.addEventListener("DOMContentLoaded", () => {
+
+ myForm.addEventListener("DOMContentLoaded", () => {
     console.log("vscode");
     localStorage.getItem();
   });
@@ -52,52 +51,91 @@ myForm.addEventListener("DOMContentLoaded", () => {
         addNewLineElement(detailsOfPeople);
       }
     }
+  } 
+
+  
+if (document.readyState !== "loading") {
+  async function getUserData() {
+    await axios.get(`https://crudcrud.com/api/6e426ee533da4525bfbd6e7524250be2/users`).then((data) => {
+      for (let i = 0; i < data.data.length; i++) {
+        console.log("creating ", data.data[i]);
+        addNewLineElement(data.data[i]);
+      }
+    });
   }
+  getUserData();
+}
 
   var keys = Object.keys(localStorage), //taking out all the keys that are there in the local storage
   i = keys.length; //6
 console.log("keys", keys);
 let stringifiedDetailsOfPeople, detailsOfPeople;
 
-function onSubmit(e){
+myForm.addEventListener('submit', async(e)=> {
     e.preventDefault();
-    
-    if(nameInput.value === '' || emailInput.value === ''){
+
+    const emailInput = document.getElementById("email").value;
+    const nameInput = document.getElementById("name").value;
+    if(nameInput === '' || emailInput === ''){
         msg.classList.add('error');
         msg.innerHTML = 'Please fill all fields';
 
         setTimeout(()  => msg.remove(), 3000)
     }
     else{
-        const emailId = document.getElementById("email").value;
-        const name = document.getElementById("name").value;
-        if (emailInput.value.length > 0 && nameInput.value.length > 0) {
-          const object = {
-            name: name,
-            emailId: emailId //unique
+        
+        if (emailInput.length > 0 && nameInput.length > 0) {
+          let object = {
+            name: nameInput,
+            emailId: emailInput //unique
           };
-          localStorage.setItem("userDetails" + emailId, JSON.stringify(object));
-          axios
-          .post('https://crudcrud.com/api/d8ce09a91dbb48ff810f441b9214bf56/users',{object})
-          .then(addNewLineElement(object));
-         // addNewLineElement(object);
+          //localStorage.setItem("userDetails" + emailId, JSON.stringify(object));
+          const response = await axios
+          .post('https://crudcrud.com/api/6e426ee533da4525bfbd6e7524250be2/users',object)
+          
+          //addNewLineElement(response.data);  
+          
+        
+          addNewLineElement(response.data);
         }
 
         function addNewLineElement(object){
         const li = document.createElement('li');
-        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
-        userList.appendChild(li);
+        const del = document.createElement('button');
+        del.className = 'del'
+        del.innerHTML = 'Delete';
+        del.style.backgroundColor = 'grey';
+        del.style.marginLeft = '20px';
+        
      
-        nameInput.value = '';
-        emailInput.value = '';
+        
+        
+        li.appendChild(document.createTextNode(`${object.name} : ${object.emailId}`));
+        li.appendChild(del);
+        userList.appendChild(li);
+       
+        del.addEventListener('click',(e)=>{
+            e.preventDefault();
+           
+            axios.delete(`https://crudcrud.com/api/6e426ee533da4525bfbd6e7524250be2/users/${object._id}`)
+            .then((res) =>{
+              userList.removeChild(li);
+            })
+          
+           
+        })
+     
+        name.value = '';
+        email.value = '';
         }
     }
-}
+});
+
 
 
 
 const getUsers = axios
-.get('https://crudcrud.com/api/d8ce09a91dbb48ff810f441b9214bf56/users').then(data => console.log(data));
+.get('https://crudcrud.com/api/6e426ee533da4525bfbd6e7524250be2/users').then(data => console.log(data));
 
 
 
